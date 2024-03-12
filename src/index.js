0694d4b1e8d989d04945f1bf3dd898658c6f4ab8
ArpitVK <vishwakarmaarpit621@gmail.com>
2024-03-12 19:57:12 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = require('./app.js')
 const mongoose = require('mongoose')
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-
+//Enable CORS for all routes
+app.use(cors());
 // Connect to DATABASE
-const DATABASE_URL = "mongodb+srv://arpitvishwakarma1314:%40Arpitvk2002@subscribers.gnfishw.mongodb.net/subscribers";
+const DATABASE_URL = "mongodb+srv://vdiksha621:Diksha%401040@mydb.cwi6q3r.mongodb.net/subscribers";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
